d3590eb36018465d5736eaf9f062c64b7697bfa0
SirvanD <sirvansh@gmail.com>
2022-05-25 13:31:04 +1000

    practice types

--- intro/dist/index.js
+++ intro/dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 let id = 5;
-console.log("ID:", id);
+let company = "Sirvan Doukchi";
+let isPublished = true;
+let x = "Hello";
+let age = 787;
+let ids = [1, 2, 3, 4, 5, 6, 7, 8, 9];
--- intro/src/index.ts
+++ intro/src/index.ts
@@ -1,2 +1,7 @@
 let id: number = 5;
-console.log("ID:", id);
+let company: string = "Sirvan Doukchi";
+let isPublished: boolean = true;
+let x: any = "Hello";
+let age: number = 787;
+
+let ids: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
